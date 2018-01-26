d50f24816c7ac70e6f00b45e3ac0a377243d52c8
Anga <angakoko@gmail.com>
2018-01-26 06:01:00 +0100

    Log Classification probability for each face

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -56,6 +56,13 @@ class Emojifier {
         // If there are no faces detected, show a Toast message
         if(faces.size() == 0){
             Toast.makeText(context, R.string.no_faces_message, Toast.LENGTH_SHORT).show();
+        }else {
+            for (int i = 0; i < faces.size(); i++){
+                Face face = faces.get(i);
+
+                //Log the classification probability for each face
+                getClassifications(face);
+            }
         }
 
         // TODO (2): Iterate through the faces, calling getClassifications() for each face.
@@ -65,4 +72,14 @@ class Emojifier {
     }
 
     // TODO (1): Create a static method called getClassifications() which logs the probability of each eye being open and that the person is smiling.
+
+    /**
+     * Method for logging the classification probability
+     * @param face
+     */
+    private static void getClassifications(Face face){
+        Log.d(LOG_TAG, "getClassifications: smilingProb = "+ face.getIsSmilingProbability());
+        Log.d(LOG_TAG, "getClassifications: leftEyeOpenProb = "+ face.getIsLeftEyeOpenProbability());
+        Log.d(LOG_TAG, "getClassifications: rightEyeOpenProb = "+ face.getIsRightEyeOpenProbability());
+    }
 }
